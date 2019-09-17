533228f0c4d3a295ad0024b3950336d14554a1c8
zhangmao <zhangmao@boya-triz.com>
2019-09-17 17:19:45 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // "off" -> 0 关闭规则
+    // "warn" -> 1 开启警告规则
+    //"error" -> 2 开启错误规则
+
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 使用双引号
+    'quotes': [1, "double"],
+    // 语句强制分号结尾
+    "semi": [1, "always"],
+    // 未定义前不能使用
+    "no-use-before-define": 2
+  }
+}
