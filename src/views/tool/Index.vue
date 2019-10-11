13a06565d263ae97452ffbb4d2a27ba75a8a627f
zhangmao <zhangmao@boya-triz.com>
2019-10-11 16:44:22 +0800

    新增json字符串格式化

--- src/views/tool/Index.vue
+++ src/views/tool/Index.vue
@@ -5,15 +5,20 @@
       <el-collapse-item title="根据excel批量生成代码" name="1">
         <generate-batch-code />
       </el-collapse-item>
+      <el-collapse-item title="JSON字符串转换成格式化的字符串" name="2">
+        <string-json-formatter />
+      </el-collapse-item>
     </el-collapse>
   </section>
 </template>
 <script>
 import GenerateBatchCode from "./util/GenerateBatchCode";
+import StringJsonFormatter from "./util/StringJsonFormatter";
 export default {
   name: "Tool",
   components: {
-    GenerateBatchCode
+    GenerateBatchCode,
+    StringJsonFormatter
   },
   data: function() {
     return {
